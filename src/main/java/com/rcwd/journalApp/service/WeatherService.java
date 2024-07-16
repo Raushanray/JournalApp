46012509eae31f1c1b4d8015160cfd977d2638b4
Raushanray <raushankuray1998@gmail.com>
2024-07-16 09:37:29 +0530

    External API  creating for hit the post call of external api in Spring Boot.

--- src/main/java/com/rcwd/journalApp/service/WeatherService.java
+++ src/main/java/com/rcwd/journalApp/service/WeatherService.java
@@ -1,7 +1,10 @@
 package com.rcwd.journalApp.service;
 
 import com.rcwd.journalApp.api.response.WeatherResponse;
+import com.rcwd.journalApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -23,5 +26,21 @@ public class WeatherService {
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         return response.getBody();
     }
+    /*
+    This method is creating for hit the post call of external api.
+     */
+
+//    public WeatherResponse getWeather1(String city) {
+//        String finalAPI = API.replace("CITY", city).replace("API_KEY", apikey);
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.set("key", "value");
+//
+//        User user = User.builder().userName("akshitakshit").password("akshit").build();
+//        HttpEntity<User> httpEntity = new HttpEntity<>(user, httpHeaders);
+//
+//        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, httpEntity, WeatherResponse.class);
+//        return response.getBody();
+//    }
 }
 
